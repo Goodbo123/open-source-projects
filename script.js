405ede94707e2c7617409e05fb7416398d43c4ec
Goodbo123 <100330857+Goodbo123@users.noreply.github.com>
2024-02-26 03:58:02 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-
+function validateForm()
+{
+  var n=document.getElementById('txt_name').value;\
+  if(n=="")
+  {
+    document.getElementById("name_validate").innerHTML="Please Enter your name"'
+  }
+  else
+  {
+    document.getElementById("name_validate").innerHTML="*";
+  }
+}
